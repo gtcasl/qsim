5b7a50def0524ae9a43c2b5a1cca0203af9f1567
chad.d.kersey <chad.d.kersey@9770eb50-c74b-b24d-123c-4e78d768fe5e>
2012-03-11 21:02:02 +0000

    Final update prior to the next tagged QSim.
    git-svn-id: https://sst-simulator.googlecode.com/svn/qsim/trunk@2667 9770eb50-c74b-b24d-123c-4e78d768fe5e

--- mkstate.sh
+++ mkstate.sh
@@ -1,15 +1,12 @@
 # Create state files for 1 through 1024 guest cores.
 
-# For now, we're making these really tiny to avoid filling up the disk. On the
-# other hand, we will probably run out of space for kernel stacks. This works
-# out to an amusingly tiny (L1 cache sized) 64kB per core for the 1024 core
-# case.
-RAMSIZE=64
+# This is the minimum needed to run Parsec with the simsmall data set.
+RAMSIZE=3072
 
 # Truncate our log file
 echo > mkstate.log
 
-for i in `seq 0 10`; do
+for i in `seq 0 6`; do
   n=`echo 2 $i ^ p | dc`
   echo "-- running qsim-fastforwarder for $n core(s) --"
   qsim-fastforwarder linux/bzImage $n $RAMSIZE state.$n 2>&1 >> mkstate.log
