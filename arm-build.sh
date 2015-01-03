4fe5ee7cbbd2ab338dd59b0aeef9cab8a1244622
Pranith Kumar <bobby.prani@gmail.com>
2015-01-03 15:21:04 -0500

    Do not set QSIM env from build script

--- arm-build.sh
+++ arm-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-export QSIM_PREFIX=/home/pranith/devops/code/qsim
 QEMU_CFLAGS="-fPIC -I/home/pranith/devops/code/qsim -shared -m64 -g" ../qemu/configure --target-list=arm-softmmu --disable-pie --enable-debug
 make -j8
