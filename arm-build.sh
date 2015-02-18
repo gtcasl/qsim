d4ca9d22514a383f6398e3fc410351b825201a2e
Pranith Kumar <bobby.prani@gmail.com>
2015-02-18 13:30:43 -0500

    User environment variable for qsim headers

--- arm-build.sh
+++ arm-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-QEMU_CFLAGS="-fPIC -I/home/pranith/devops/code/qsim -shared -m64 -g" ../qemu/configure --target-list=arm-softmmu --disable-pie --enable-debug
+QEMU_CFLAGS="-fPIC -I${QSIM_PREFIX} -shared -m64 -g" ../qemu/configure --target-list=arm-softmmu --disable-pie --enable-debug
 make -j8
