9be209a4d9ded0b0b8dc2d661bc0ef1504b7b8ba
Pranith Kumar <bobby.prani@gmail.com>
2015-06-03 18:50:23 -0400

    Macsim trace info

--- arm-examples/macsim_tracegen.cpp
+++ arm-examples/macsim_tracegen.cpp
@@ -18,10 +18,249 @@
 
 #include "gzstream.h"
 #include "cs_disas.h"
+#include "macsim_tracegen.h"
 
 using Qsim::OSDomain;
 
 using std::ostream;
+
+class InstHandler {
+public:
+    InstHandler() {}
+    InstHandler(ogzstream *outfile);
+    void setOutFile(ogzstream *outfile);
+    bool populateInst(cs_insn *insn);
+private:
+    trace_info_cpu_s op; 
+    ogzstream *outfile;
+};
+
+InstHandler::InstHandler(ogzstream *out)
+{
+    outfile = out;
+}
+
+void InstHandler::setOutFile(ogzstream *out)
+{
+    outfile = out;
+}
+
+static uint8_t get_xed_opcode(unsigned int inst)
+{
+    uint8_t ret;
+    switch (inst) {
+        case 0: 
+            ret = XED_CATEGORY_INVALID;
+		
+            ret = XED_CATEGORY_3DNOW;
+		case ARM64_INS_AESD:
+		case ARM64_INS_AESE:
+		case ARM64_INS_AESIMC:
+		case ARM64_INS_AESMC:
+            ret = XED_CATEGORY_AES;
+		
+            ret = XED_CATEGORY_AVX;
+		
+            ret = XED_CATEGORY_AVX2; // new
+		
+            ret = XED_CATEGORY_AVX2GATHER; // new
+		
+            ret = XED_CATEGORY_BDW; // new
+		
+            ret = XED_CATEGORY_BINARY;
+		
+            ret = XED_CATEGORY_BITBYTE;
+		
+            ret = XED_CATEGORY_BMI1; // new
+		
+            ret = XED_CATEGORY_BMI2; // new
+		
+            ret = XED_CATEGORY_BROADCAST;
+		
+            ret = XED_CATEGORY_CALL;
+		
+            ret = XED_CATEGORY_CMOV;
+            ret = XED_CATEGORY_COND_BR;
+		
+            ret = XED_CATEGORY_CONVERT;
+		
+            ret = XED_CATEGORY_DATAXFER;
+		
+            ret = XED_CATEGORY_DECIMAL;
+		
+            ret = XED_CATEGORY_FCMOV;
+		
+            ret = XED_CATEGORY_FLAGOP;
+		
+            ret = XED_CATEGORY_FMA4; // new
+		
+            ret = XED_CATEGORY_INTERRUPT;
+		
+            ret = XED_CATEGORY_IO;
+		
+            ret = XED_CATEGORY_IOSTRINGOP;
+		case ARM64_INS_ABS:
+        case ARM64_INS_ADC:
+        case ARM64_INS_ADDHN:
+        case ARM64_INS_ADDHN2:
+        case ARM64_INS_ADDP:
+        case ARM64_INS_ADD:
+        case ARM64_INS_ADDV:
+        case ARM64_INS_ADR:
+        case ARM64_INS_ADRP:
+        case ARM64_INS_AND:
+            ret = XED_CATEGORY_LOGICAL;
+		
+            ret = XED_CATEGORY_LZCNT; // new
+		
+            ret = XED_CATEGORY_MISC;
+		
+            ret = XED_CATEGORY_MMX;
+		
+            ret = XED_CATEGORY_NOP;
+		
+            ret = XED_CATEGORY_PCLMULQDQ;
+		
+            ret = XED_CATEGORY_POP;
+		
+            ret = XED_CATEGORY_PREFETCH;
+		
+            ret = XED_CATEGORY_PUSH;
+		
+            ret = XED_CATEGORY_RDRAND; // new
+		
+            ret = XED_CATEGORY_RDSEED; // new
+		
+            ret = XED_CATEGORY_RDWRFSGS; // new
+		
+            ret = XED_CATEGORY_RET;
+		
+            ret = XED_CATEGORY_ROTATE;
+		
+            ret = XED_CATEGORY_SEGOP;
+		
+            ret = XED_CATEGORY_SEMAPHORE;
+        case ARM64_INS_ASR:
+            ret = XED_CATEGORY_SHIFT;
+		
+            ret = XED_CATEGORY_SSE;
+		
+            ret = XED_CATEGORY_STRINGOP;
+		
+            ret = XED_CATEGORY_STTNI;
+		
+            ret = XED_CATEGORY_SYSCALL;
+		
+            ret = XED_CATEGORY_SYSRET;
+		
+            ret = XED_CATEGORY_SYSTEM;
+		
+            ret = XED_CATEGORY_TBM; // new
+        case ARM64_INS_B:
+            ret = XED_CATEGORY_UNCOND_BR;
+		
+            ret = XED_CATEGORY_VFMA; // new
+		
+            ret = XED_CATEGORY_VTX;
+		
+            ret = XED_CATEGORY_WIDENOP;
+		
+            ret = XED_CATEGORY_X87_ALU;
+		
+            ret = XED_CATEGORY_XOP;
+		
+            ret = XED_CATEGORY_XSAVE;
+		
+            ret = XED_CATEGORY_XSAVEOPT;
+		
+            ret = TR_MUL;
+		
+            ret = TR_DIV;
+		
+            ret = TR_FMUL;
+		
+            ret = TR_FDIV;
+		
+            ret = TR_NOP;
+		
+            ret = PREFETCH_NTA;
+		
+            ret = PREFETCH_T0;
+		
+            ret = PREFETCH_T1;
+		
+            ret = PREFETCH_T2;
+		
+        default:
+            ret = XED_CATEGORY_INVALID;
+    }
+
+    return ret;
+}
+
+bool InstHandler::populateInst(cs_insn *insn)
+{
+    cs_arm64* arm64;
+
+    if (insn->detail == NULL)
+        return false;
+
+    arm64 = &(insn->detail->arm64);
+
+    op.m_num_read_regs = insn->detail->regs_read_count;
+    op.m_num_dest_regs = insn->detail->regs_write_count;
+    op.m_size = 4;
+    op.m_instruction_addr  = insn->address;
+
+    for (int i = 0; i < op.m_num_read_regs; i++)
+        op.m_src[i] = insn->detail->regs_read[i];
+    for (int i = 0; i < op.m_num_dest_regs; i++)
+        op.m_dst[i] = insn->detail->regs_write[i];
+
+    op.m_cf_type = 0;
+    for (int grp_idx = 0; grp_idx < insn->detail->groups_count; grp_idx++) {
+        if (insn->detail->groups[grp_idx] == ARM64_GRP_JUMP) {
+            op.m_cf_type = 1;
+            break;
+        }
+    }
+
+    op.m_has_immediate = 0;
+    for (int op_idx = 0; op_idx < arm64->op_count; op_idx++) {
+        if (arm64->operands[op_idx].type == ARM64_OP_IMM ||
+            arm64->operands[op_idx].type == ARM64_OP_CIMM) {
+            op.m_has_immediate = 1;
+            break;
+        }
+    }
+
+    op.m_has_st = 0;
+    if (ARM64_INS_ST1 <= insn->id && insn->id <= ARM64_INS_STXR)
+        op.m_has_st = 1;
+
+    op.m_is_fp = 0;
+    for (int op_idx = 0; op_idx < arm64->op_count; op_idx++) {
+        if (arm64->operands[op_idx].type == ARM64_OP_FP) {
+            op.m_is_fp = 1;
+            break;
+        }
+    }
+
+    op.m_write_flg  = arm64->writeback;
+    op.m_num_ld = 0;
+    op.m_ld_vaddr2 = 0;
+    op.m_st_vaddr = 0;
+    op.m_branch_target = 0;
+    op.m_mem_read_size = 0;
+    op.m_mem_write_size = 0;
+    op.m_rep_dir = 0;
+    op.m_actually_taken = 0;
+    op.m_opcode = get_xed_opcode(insn->id);
+
+	for (int i = 0; i < arm64->op_count; i++) {
+		cs_arm64_op *op = &(arm64->operands[i]);
+    }
+}
 
 class TraceWriter {
 public:
@@ -40,9 +279,11 @@ public:
     if (!ran) {
       ran = true;
       osd.set_inst_cb(this, &TraceWriter::inst_cb);
+      osd.set_mem_cb(this, &TraceWriter::mem_cb);
       osd.set_app_end_cb(this, &TraceWriter::app_end_cb);
     }
     tracefile = new ogzstream(("trace_" + std::to_string(trace_file_count) + ".log.gz").c_str());
+    inst_handle.setOutFile(tracefile);
     trace_file_count++;
     finished = false;
 
@@ -55,10 +296,10 @@ public:
       finished = true;
 
       if (tracefile) {
-              fflush(NULL);
           tracefile->close();
           delete tracefile;
           tracefile = NULL;
+          inst_handle.setOutFile(tracefile);
       }
 
       return 0;
@@ -86,12 +327,18 @@ public:
       return;
   }
 
+  int mem_cb(int c, uint64_t v, uint64_t p, uint8_t s, int w)
+  {
+      std::cout << "v: 0x" << std::hex << v << " p: 0x" << std::hex << p << std::endl;
+  }
+
 private:
   cs_disas dis;
   OSDomain &osd;
   ogzstream* tracefile;
   bool finished;
   int  trace_file_count;
+  InstHandler inst_handle;
 
   static const char * itype_str[];
 };
