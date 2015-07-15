c0c11aceff19b34bfa812750e364674b3a3fe80b
Pranith Kumar <bobby.prani@gmail.com>
2015-07-15 02:38:12 -0400

    Fix build script and add library copy

--- arm64-build.sh
+++ arm64-build.sh
@@ -7,9 +7,9 @@ else
   exit 0;
 fi
 
-if [[ $1 -ne "release" ]]; then
-  $debug_flags = "--enable-debug --enable-debug-tcg --enable-debug-info"
+if [ -z $1 ]; then
+  debug_flags="--enable-debug --enable-debug-tcg --enable-debug-info"
 fi
 
 QEMU_CFLAGS="-I${QSIM_PREFIX} -shared -m64 -g  -Werror -fPIC -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wall -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wold-style-declaration -Wold-style-definition -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure --target-list=aarch64-softmmu --disable-pie $debug_flags
-make -j16
+make -j16 && cp aarch64-softmmu/qemu-system-aarch64 $QSIM_PREFIX/lib/libqemu-qsim.so
