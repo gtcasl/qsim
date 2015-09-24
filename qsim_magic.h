b289c0b47f34fb33632540085d707e849a2f7431
Pranith Kumar <bobby.prani@gmail.com>
2015-09-24 15:53:12 -0400

    Update magic with signal

--- qsim_magic.h
+++ qsim_magic.h
@@ -1,6 +1,9 @@
 #ifndef _QSIM_MAGIC_H
 #define _QSIM_MAGIC_H
 
+#include <signal.h>
+#include <stdlib.h>
+
 #define qsim_magic_enable()				\
 do {							\
 	asm volatile (  "1:mrs %0, DCZID_EL0\n\t"	\
@@ -13,5 +16,21 @@ do {							\
 } while(0);
 
 #define qsim_magic_disable() qsim_magic_enable()
+
+static void qsim_sig_handler(int signo)
+{
+	char c;
+	qsim_magic_disable();
+	printf("Callbacks disabled. Quit(y/n)?");
+	scanf("%c", &c);
+
+	if (c == 'y')
+		exit(0);
+}
+
+#define qsim_init()					\
+do {							\
+	signal(SIGINT, qsim_sig_handler);		\
+} while(0);
 
 #endif /* _QSIM_MAGIC_H */
