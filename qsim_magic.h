a1c3f526079cb3b4bd128917a1389d9d915951e7
Pranith Kumar <bobby.prani@gmail.com>
2015-11-11 11:49:56 -0500

    Update magic header file

--- qsim_magic.h
+++ qsim_magic.h
@@ -4,10 +4,24 @@
 #include <signal.h>
 #include <stdlib.h>
 
+#if defined(__arm__) || defined(__aarch64__)
+
 #define qsim_magic_enable()				\
 	asm volatile("msr pmcr_el0, %0" :: "r" (0xaaaaaaaa));
 #define qsim_magic_disable() 				\
 	asm volatile("msr pmcr_el0, %0" :: "r" (0xfa11dead));
+
+#elif defined(__i386__) || defined(__x86_64__)
+
+#define qsim_magic_enable()				\
+	asm volatile("cpuid;"::"a"(0xaaaaaaaa));
+#define qsim_magic_disable()				\
+	asm volatile("cpuid;"::"a"(0xfa11dead));
+
+#endif
+
+#define APP_START() qsim_magic_enable()
+#define APP_END()   qsim_magic_disable()
 
 static void qsim_sig_handler(int signo)
 {
