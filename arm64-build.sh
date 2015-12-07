7ac7f54c31ddfe3f143f5708e416390f2792e7d1
Pranith Kumar <bobby.prani@gmail.com>
2015-12-07 16:36:12 -0500

    Update the build script for unified qemu build

--- arm64-build.sh
+++ arm64-build.sh
@@ -12,10 +12,7 @@ if [ -z $1 ]; then
 fi
 
 mkdir -p $QSIM_PREFIX/lib/
-QEMU_CFLAGS="-DQSIM_ARM64 -I${QSIM_PREFIX} -m64 -g -Werror -fPIC -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wall -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure --extra-ldflags=-shared --target-list=aarch64-softmmu --disable-pie $debug_flags
+QEMU_CFLAGS="-I${QSIM_PREFIX} -m64 -g -Werror -fPIC -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wall -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure --extra-ldflags=-shared --target-list=aarch64-softmmu,x86_64-softmmu --disable-pie $debug_flags
 make -j16
 cp aarch64-softmmu/qemu-system-aarch64 $QSIM_PREFIX/lib/libqemu-qsim-a64.so
-rm -f vl.o
-QEMU_CFLAGS="-DQSIM_X86 -I${QSIM_PREFIX} -m64 -g -Werror -fPIC -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wall -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure --extra-ldflags=-shared --target-list=x86_64-softmmu --disable-pie $debug_flags
-make -j16
 cp x86_64-softmmu/qemu-system-x86_64 $QSIM_PREFIX/lib/libqemu-qsim-x86.so
