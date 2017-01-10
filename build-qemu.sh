d26a3d6c64320433b43b2f28e8432bbdbf22027a
Pranith Kumar <bobby.prani@gmail.com>
2017-01-10 16:20:45 -0500

    Update qemu build arguments

--- build-qemu.sh
+++ build-qemu.sh
@@ -20,7 +20,22 @@ mkdir -p $QSIM_PREFIX/lib/
 if [ ! -d "$build_dir" ]; then
   mkdir -p $build_dir
   cd $build_dir
-  QEMU_CFLAGS="-I${QSIM_PREFIX} -g -fPIC -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wall -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure --extra-ldflags=-shared --target-list=aarch64-softmmu,x86_64-softmmu --disable-pie --disable-brlapi --disable-rdma --disable-rbd --disable-tcmalloc --disable-xen --disable-gtk --disable-uuid --disable-glusterfs --disable-xfsctl $debug_flags
+  QEMU_CFLAGS="-I${QSIM_PREFIX} -g -fPIC -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 \
+  -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wall -Wundef     \
+  -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common       \
+  -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs         \
+  -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers             \
+  -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure   \
+             --extra-ldflags=-shared --disable-libssh2 --disable-tcmalloc \
+             --disable-seccomp --disable-{bzip2,snappy,lzo} --disable-usb-redir \
+             --disable-libusb --disable-libnfs  \
+             --disable-libiscsi --disable-rbd  --disable-spice --disable-attr \
+             --disable-cap-ng --disable-linux-aio --disable-uuid --disable-brlapi \
+             --disable-vnc-{jpeg,sasl,png} --disable-rdma --disable-bluez \
+             --disable-curl --disable-curses --disable-sdl \
+             --disable-gtk  --disable-tpm --disable-vte --disable-vnc  \
+             --disable-xen --disable-opengl --target-list=x86_64-softmmu,aarch64-softmmu \
+             --disable-glusterfs --disable-xfsctl $debug_flags
 else
   cd $build_dir
 fi
