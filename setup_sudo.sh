b1e33516426663f277a5da5b56b2b59d28fa5099
Pranith Kumar <bobby.prani@gmail.com>
2018-09-11 22:22:02 +0000

    Update setup_sudo script

--- setup_sudo.sh
+++ setup_sudo.sh
@@ -10,39 +10,39 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-ARCH=$1
+ARCH=`uname -m`
 
 # setup the aarch64 toolchain
 aarch64_tool=$PWD/tools/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu
 
 if command -v aarch64-linux-gnu-gcc >/dev/null 2>&1 ; then
-    echo "AARCH64 toolchain is already installed!"
+    echo -e "AARCH64 toolchain is already installed!"
 else
-    echo "\nAARCH64 toolchain is not set, download from linaro website..."
-    echo "Press any key to continue..."
+    echo -e "\nAARCH64 toolchain is not set, download from linaro website..."
+    echo -e "Press any key to continue..."
     read inp
     mkdir -p tools
     cd tools 
     wget -c "https://github.com/gtcasl/qsim_prebuilt/releases/download/v0.1/aarch64_toolchain.tar.xz"
-    echo "\nUncompressing the toolchain..."
+    echo -e "\nUncompressing the toolchain..."
     tar -xf aarch64_toolchain.tar.xz
     export PATH="$PATH:$aarch64_tool/bin"
-    echo "\n\nAdd the following lines to your bashrc:\n"
-    echo "${bold}export PATH=\$PATH:\$QSIM_PREFIX/tools/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu/bin${normal}"
+    echo -e "\n\nAdd the following lines to your bashrc:\n"
+    echo -e "${bold}export PATH=\$PATH:\$QSIM_PREFIX/tools/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu/bin${normal}"
     cd ..
 fi
 
 # set the QSIM environment variable
-echo "Setting QSIM environment variable..."
+echo -e "Setting QSIM environment variable..."
 export QSIM_PREFIX=`pwd`
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$QSIM_PREFIX/lib
-echo "\n\nAdd the following lines to your bashrc:\n"
-echo "${bold}export QSIM_PREFIX=$QSIM_PREFIX${bold}"
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$QSIM_PREFIX/lib${normal}\n"
-echo "Press any key to continue..."
+echo -e "\n\nAdd the following lines to your bashrc:\n"
+echo -e "${bold}export QSIM_PREFIX=$QSIM_PREFIX${bold}"
+echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$QSIM_PREFIX/lib${normal}\n"
+echo -e "Press any key to continue..."
 read inp
 
-echo "\n\nDown QEMU OS images? This will take a while. (Y/n):"
+echo -e "\n\nDown QEMU OS images? This will take a while. (y/N):"
 read inp
 
 # Install dependencies
@@ -58,15 +58,14 @@ else
 fi
 
 if [ "$inp" = "y" -o "$inp" = "Y" ]; then
-  cd ..
   # get qemu images
-  echo "\nDownloading arm QEMU images..."
+  echo -e "\nDownloading arm QEMU images..."
   mkdir -p images
   cd images
   wget -c https://github.com/gtcasl/qsim_prebuilt/releases/download/v0.1/arm64_images.tar.xz
   wget -c https://github.com/gtcasl/qsim_prebuilt/releases/download/v0.1/x86_64_images.tar.xz
 
-  echo "\nUncompresssing images. This might take a while..."
+  echo -e "\nUncompresssing images. This might take a while..."
   tar -xf arm64_images.tar.xz
   tar -xf x86_64_images.tar.xz
   cd $QSIM_PREFIX
@@ -88,24 +87,28 @@ git submodule update --init dtc
 cd $QSIM_PREFIX
 
 # build linux kernel and initrd
-echo "Building Linux kernel..."
+echo -e "Building Linux kernel..."
 cd linux
 ./getkernel.sh
-./getkernel.sh arm64
+if [ "$HOST" != "aarch64" ]; then
+  ./getkernel.sh arm64
+fi
 cd $QSIM_PREFIX
 
 # build qemu
-echo "\nConfiguring and building qemu...\n"
+echo -e "\nConfiguring and building qemu...\n"
 ./build-qemu.sh release
 
 # build qsim
 # copy header files to include directory
 make release install
 
-echo "\n\nBuilding busybox"
+echo -e "\n\nBuilding busybox"
 cd initrd/
 ./getbusybox.sh
-./getbusybox.sh arm64
+if [ "$HOST" != "aarch64" ]; then
+  ./getbusybox.sh arm64
+fi
 cd $QSIM_PREFIX
 
 # run tests
@@ -116,5 +119,5 @@ make tests
 # make && ./cachesim
 
 if [ $? -eq "0" ]; then
-  echo "\n${bold}Setup finished successfully!${normal}"
+  echo -e "\n${bold}Setup finished successfully!${normal}"
 fi
